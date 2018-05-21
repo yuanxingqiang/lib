5ea99a9834fab4224fe524613ac18efd2a0193c5
Yuanxingqian <844800670@qq.com>
2018-05-21 15:52:45 +0800

    第一次增加函数
    增加了两个获取文件拓展名的字符串

--- documentTools.php
+++ documentTools.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * 文件操作函数库
+ * @author Yuan
+ * @time 2018-5-21 15:35:00
+ */
+
+/**
+ * 输入文件名（可带路径），获取改文件的拓展名（之一）
+ * @param string $fileName 文件的名称（可带路径）
+ * @return string  文件的拓展名
+ * @author Yuan
+ * @time 2018-5-21 15:38:00
+ */
+function getFileExtName1($fileName){
+    return strtolower(pathinfo($fileName,PATHINFO_EXTENSION));
+}
+
+
+/**
+ * 输入文件名（可带路径），获取文件的拓展名（之二）
+ * @param string $fileName 文件的名称（可带路径）
+ * @return string 文件的拓展名
+ * @author Yuan
+ * @time 2018-5-21 15:49:00
+ */
+function getFileExtName2($fileName){
+    return strtolower(substr($fileName,strripos(SfileName,'.')));
+}
